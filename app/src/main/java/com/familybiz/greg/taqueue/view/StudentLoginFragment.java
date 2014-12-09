f061db40c16eaf211b782e5e802261f96f7d5612
Greg Anderson <greg@infobytes.com>
2014-12-09 03:33:08 -0700

    Added a listener that is called on success.

--- app/src/main/java/com/familybiz/greg/taqueue/view/StudentLoginFragment.java
+++ app/src/main/java/com/familybiz/greg/taqueue/view/StudentLoginFragment.java
@@ -6,16 +6,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.familybiz.greg.taqueue.R;
+import com.familybiz.greg.taqueue.model.Student;
+import com.familybiz.greg.taqueue.network.StudentRequest;
 
 /**
  * Represents the student login screen.
  *
  * Created by Greg Anderson
  */
-public class StudentLoginFragment extends LoginFragment {
+public class StudentLoginFragment extends LoginFragment implements StudentRequest.OnStudentCreatedListener {
+
+	// Network
+	private StudentRequest mStudentRequest;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		mStudentRequest = new StudentRequest();
+		mStudentRequest.setOnStudentCreatedListener(this);
+
 		View rootLayout = super.onCreateView(inflater, container, savedInstanceState);
 		mNameTextBox.setHint(getString(R.string.name_hint));
 		mPasswordTextBox.setHint(getString(R.string.location_hint));
@@ -25,5 +33,25 @@ public class StudentLoginFragment extends LoginFragment {
 	@Override
 	void makeNetworkCallToCreateUser() {
 		// TODO: Implement
+	}
+
+	@Override
+	public void onStudentCreated(Student student) {
+		if (mOnStudentLoginSuccessListener != null)
+			mOnStudentLoginSuccessListener.onStudentLoginSuccess();
+	}
+
+
+	/***************************** LISTENERS *****************************/
+
+
+	public interface OnStudentLoginSuccessListener {
+		public void onStudentLoginSuccess();
+	}
+
+	private OnStudentLoginSuccessListener mOnStudentLoginSuccessListener;
+
+	public void setOnStudentLoginSuccessListener(OnStudentLoginSuccessListener onStudentLoginSuccessListener) {
+		mOnStudentLoginSuccessListener = onStudentLoginSuccessListener;
 	}
 }
