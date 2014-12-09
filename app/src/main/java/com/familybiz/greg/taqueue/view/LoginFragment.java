eaa834fed720a58575229ad4f057b7300c82b95f
Greg Anderson <greg@infobytes.com>
2014-12-09 00:37:45 -0700

    Keyboard now hides when the user clicks login button

--- app/src/main/java/com/familybiz/greg/taqueue/view/LoginFragment.java
+++ app/src/main/java/com/familybiz/greg/taqueue/view/LoginFragment.java
@@ -1,12 +1,14 @@
 package com.familybiz.greg.taqueue.view;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -28,12 +30,10 @@ public abstract class LoginFragment extends Fragment {
 	// There is probably a better name for it, but I couldn't come up with it.
 	protected EditText mPasswordTextBox;
 
-	private LinearLayout mRootLayout;
-
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		mRootLayout = new LinearLayout(getActivity());
-		mRootLayout.setOrientation(LinearLayout.VERTICAL);
+		LinearLayout rootLayout = new LinearLayout(getActivity());
+		rootLayout.setOrientation(LinearLayout.VERTICAL);
 
 		mNameTextBox = new EditText(getActivity());
 		mPasswordTextBox = new EditText(getActivity());
@@ -47,11 +47,11 @@ public abstract class LoginFragment extends Fragment {
 		Button loginButton = new Button(getActivity());
 		loginButton.setText(getString(R.string.login_button));
 
-		mRootLayout.addView(mNameTextBox, new LinearLayout.LayoutParams(
+		rootLayout.addView(mNameTextBox, new LinearLayout.LayoutParams(
 				ViewGroup.LayoutParams.MATCH_PARENT,
 				ViewGroup.LayoutParams.WRAP_CONTENT));
 
-		mRootLayout.addView(mPasswordTextBox, new LinearLayout.LayoutParams(
+		rootLayout.addView(mPasswordTextBox, new LinearLayout.LayoutParams(
 				ViewGroup.LayoutParams.MATCH_PARENT,
 				ViewGroup.LayoutParams.WRAP_CONTENT));
 
@@ -59,18 +59,22 @@ public abstract class LoginFragment extends Fragment {
 				ViewGroup.LayoutParams.WRAP_CONTENT,
 				ViewGroup.LayoutParams.WRAP_CONTENT);
 		buttonParams.gravity = Gravity.CENTER_HORIZONTAL;
-		mRootLayout.addView(loginButton, buttonParams);
+		rootLayout.addView(loginButton, buttonParams);
 
 
 		loginButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
+				// Hide the keyboard
+				InputMethodManager inputMethodManager = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+				inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
 				ProgressBar loadingCircle = (ProgressBar)getActivity().findViewById(R.id.loading_circle);
 				loadingCircle.setVisibility(View.VISIBLE);
 				// TODO: Make network call
 			}
 		});
 
-		return mRootLayout;
+		return rootLayout;
 	}
 }
