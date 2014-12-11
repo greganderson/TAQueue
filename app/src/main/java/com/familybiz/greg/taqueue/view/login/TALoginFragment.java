459a8dfa1dde8478053ab42ab58d3d366ca42259
Greg Anderson <greg@infobytes.com>
2014-12-11 14:44:03 -0700

    Can't remember the changes here.

--- app/src/main/java/com/familybiz/greg/taqueue/view/login/TALoginFragment.java
+++ app/src/main/java/com/familybiz/greg/taqueue/view/login/TALoginFragment.java
@@ -6,16 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.familybiz.greg.taqueue.R;
+import com.familybiz.greg.taqueue.model.TA;
+import com.familybiz.greg.taqueue.network.TARequest;
 
 /**
  * Represents the TA login screen.
  *
  * Created by Greg Anderson
  */
-public class TALoginFragment extends LoginFragment {
+public class TALoginFragment extends LoginFragment implements TARequest.OnTACreatedListener {
+
+	private TARequest mTARequest;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		mTARequest = new TARequest();
+		mTARequest.setOnTACreatedListener(this);
+
 		View rootLayout = super.onCreateView(inflater, container, savedInstanceState);
 		mNameTextBox.setHint(getString(R.string.name_hint));
 		mPasswordTextBox.setHint(getString(R.string.password_hint));
@@ -24,6 +31,26 @@ public class TALoginFragment extends LoginFragment {
 
 	@Override
 	void makeNetworkCallToCreateUser(String name, String password) {
-		// TODO: Implement
+		mTARequest.createTA(name, password);
+	}
+
+	@Override
+	public void onTACreated(TA ta) {
+		if (mOnTALoginSuccessListener != null)
+			mOnTALoginSuccessListener.onTALoginSuccess(ta);
+	}
+
+
+	/***************************** LISTENERS *****************************/
+
+
+	public interface OnTALoginSuccessListener {
+		public void onTALoginSuccess(TA ta);
+	}
+
+	private OnTALoginSuccessListener mOnTALoginSuccessListener;
+
+	public void setOnTALoginSuccessListener(OnTALoginSuccessListener onTALoginSuccessListener) {
+		mOnTALoginSuccessListener = onTALoginSuccessListener;
 	}
 }
